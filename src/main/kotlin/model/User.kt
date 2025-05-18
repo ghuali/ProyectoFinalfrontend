2c33a7c5c40b02569e3ed8a00ef547bf9486de0c
ghuali <guaghuali@gmail.com>
2025-05-18 18:44:25 +0100

    Creado User

---
+++ src/main/kotlin/model/User.kt
@@ -0,0 +1,11 @@
+package model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    val id: Int,
+    val nombre: String,
+    val rol: String,
+    val email: String
+)
