400201015f417a199965060a75f28b88dc9be6f3
ghuali <guaghuali@gmail.com>
2025-05-18 18:44:45 +0100

    Cambiado ApiResponse.kt

--- src/main/kotlin/network/ApiResponse.kt
+++ src/main/kotlin/network/ApiResponse.kt
@@ -1,29 +1,35 @@
-//package network
-//import model.LoginRequest
-//import network.NetworkUtils.httpClient
-//import io.ktor.client.call.*
-//import io.ktor.client.request.*
-//import io.ktor.client.statement.*
-//import io.ktor.http.*
-//import kotlinx.coroutines.CoroutineScope
-//import kotlinx.coroutines.Dispatchers
-//import kotlinx.coroutines.launch
-//import utils.sha512
-//
-//
-//fun apiLogIn(usuario: String, password: String, onSucessResponse: (User) -> Unit) {
-//    val url = "http://127.0.0.1:5000/gestor/login"
-//    CoroutineScope(Dispatchers.IO).launch {
-//        val response = httpClient.post(url){
-//            contentType(ContentType.Application.Json)
-//            setBody(LoginRequest(usuario, sha512(password)))
-//        }
-//
-//    if (response.status == HttpStatusCode.OK) {
-//        val user = response.body<User>()
-//        onSucessResponse(user)
-//    } else {
-//        println("Error: ${response.status}, Body: ${response.bodyAsText()}")
-//    }
-//}
-//}
+package network
+import model.LoginRequest
+import network.NetworkUtils.httpClient
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import model.User
+import utils.sha512
+
+
+fun apiLogIn(email: String, password: String, onSucessResponse: (User) -> Unit) {
+    val url = "http://127.0.0.1:5000/usuario/login"
+    CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = httpClient.post(url) {
+                contentType(ContentType.Application.Json)
+                setBody(LoginRequest(email, password))
+            }
+
+            if (response.status == HttpStatusCode.OK) {
+                val user = response.body<User>()
+                onSucessResponse(user)
+            } else {
+                println("Error: ${response.status}, Body: ${response.bodyAsText()}")
+            }
+        } catch (e: Exception) {
+            println("Excepción en login: ${e.message}")
+        }
+    }
+}
+
