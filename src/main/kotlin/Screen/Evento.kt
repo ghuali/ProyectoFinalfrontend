6025bbdc862677940f5afd25a1e9a0c610ce299d
ghuali <guaghuali@gmail.com>
2025-05-22 13:45:17 +0100

    Quitado imports

--- src/main/kotlin/Screen/Evento.kt
+++ src/main/kotlin/Screen/Evento.kt
@@ -10,8 +10,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
