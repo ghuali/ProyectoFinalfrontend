2587e4d601bf761e20fee1c093e3ef4018813607
ghuali <guaghuali@gmail.com>
2025-05-23 12:50:07 +0100

    Cambiado un poco de Logica del http

--- src/main/kotlin/network/NetworkUtils.kt
+++ src/main/kotlin/network/NetworkUtils.kt
@@ -2,15 +2,13 @@ package network
 
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
-
 object NetworkUtils {
-    val httpClient = HttpClient{
-        install(ContentNegotiation){
-            json(json = Json{ ignoreUnknownKeys = true}, contentType = ContentType.Any)
+    val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true }) // <- aquí el cambio importante
         }
     }
 }
