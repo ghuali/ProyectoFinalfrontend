43367846f7983741fe17df5e7bcc629fa468a888
ghuali <guaghuali@gmail.com>
2025-05-24 14:56:27 +0100

    Actualicion de Juego dataclass

--- src/main/kotlin/model/Juego.kt
+++ src/main/kotlin/model/Juego.kt
@@ -6,7 +6,5 @@ import kotlinx.serialization.Serializable
 data class Juego(
     val id_juego: Int,
     val nombre: String,
-    val descripcion: String,
-    val plataforma: String,
     val es_individual: Boolean
 )
